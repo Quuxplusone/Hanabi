497d8ef8e5c8e9734893a337f8614d345eadc5ca
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2013-04-24 16:31:36 -0700

    Add a much better "main.cc" harness.
    The run_FooBot harness now comes with command-line argument parsing:
    you can pass "--quiet" to suppress the initial logging spew, or
    "--players", "--games", or "--every" to affect the number of players,
    total number of games played, and frequency of statistical spew,
    respectively.

--- main.cc
+++ main.cc
@@ -14,42 +14,122 @@
 #include stringify(BOTNAME.h)  /* This isn't really Standard-conforming. */
 #include "BotFactory.h"
 
-int main()
-{
-    std::srand(std::time(NULL));
+struct Statistics {
+    int games;
+    int totalScore;
+    int perfectGames;
+    int mulligansUsed[4];
+};
 
+static void run_iterations(int numberOfPlayers, int iterations, Statistics &global_stats)
+{
     Hanabi::Server server;
     BotFactory<BOTNAME> botFactory;
+    Statistics local_stats = {};
 
-    server.setLog(&std::cerr);
-
-    int sum = 0;
-    int perfectGames = 0;
-    int mulligansUsed[4] = {};
-
-    for (int i=1; i <= 1000000; ++i) {
-        int score = server.runGame(botFactory, 3);
+    for (int i=0; i < iterations; ++i) {
+        int score = server.runGame(botFactory, numberOfPlayers);
         assert(score == server.currentScore());
         assert(0 <= server.mulligansUsed() && server.mulligansUsed() <= 3);
-        sum += score;
-        perfectGames += (score == 25);
-        mulligansUsed[server.mulligansUsed()] += 1;
-        if (i == 1) {
-            std::cout << stringify(BOTNAME) " scored " << score << " points in that first game.\n";
-            server.setLog(NULL);
-        } else if ((i % 20000) == 0) {
-            std::cout << "Over " << i << " games, " stringify(BOTNAME) " scored an average of "
-                      << (sum / (double)i) << " points per game.\n";
-            if (perfectGames != 0) {
-                std::cout << "  " << 100*(perfectGames / (double)i) << " percent were perfect games.\n";
-            }
-            if (mulligansUsed[0] != i) {
-                std::cout << "  Mulligans used: 0 (" << 100*(mulligansUsed[0] / (double)i)
-                          << "%); 1 (" << 100*(mulligansUsed[1] / (double)i)
-                          << "%); 2 (" << 100*(mulligansUsed[2] / (double)i)
-                          << "%); 3 (" << 100*(mulligansUsed[3] / (double)i) << "%).\n";
-            }
+        local_stats.totalScore += score;
+        local_stats.perfectGames += (score == 25);
+        local_stats.mulligansUsed[server.mulligansUsed()] += 1;
+    }
+
+    /* Ideally this stat-summation would be an atomic transaction. */
+    global_stats.games += iterations;
+    global_stats.totalScore += local_stats.totalScore;
+    global_stats.perfectGames += local_stats.perfectGames;
+    for (int i=0; i < 4; ++i) {
+        global_stats.mulligansUsed[i] += local_stats.mulligansUsed[i];
+    }
+}
+
+static void dump_stats(Statistics stats)
+{
+    double dgames = stats.games;
+
+    std::cout << "Over " << stats.games << " games, " stringify(BOTNAME) " scored an average of "
+              << (stats.totalScore / dgames) << " points per game.\n";
+    if (stats.perfectGames != 0) {
+        std::cout << "  " << 100*(stats.perfectGames / dgames) << " percent were perfect games.\n";
+    }
+    if (stats.mulligansUsed[0] != stats.games) {
+        std::cout << "  Mulligans used: 0 (" << 100*(stats.mulligansUsed[0] / dgames)
+                  << "%); 1 (" << 100*(stats.mulligansUsed[1] / dgames)
+                  << "%); 2 (" << 100*(stats.mulligansUsed[2] / dgames)
+                  << "%); 3 (" << 100*(stats.mulligansUsed[3] / dgames) << "%).\n";
+    }
+}
+
+static void usage(const char *why)
+{
+    std::cout << why << "\n"
+              << "Usage:\n"
+              << "    ./run_" stringify(BOTNAME) "Bot [options]\n"
+              << "        --quiet\n"
+              << "        --players <2..10>\n"
+              << "        --games <1000..2000000000>\n"
+              << "        --every <1000..2000000000>\n";
+    std::exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv)
+{
+    int numberOfPlayers = 3;
+    int numberOfGames = 1000*1000;
+    int every = 20000;
+    bool logOneGame = true;
+
+    for (int i=1; i < argc; ++i) {
+        if (argv[i] == std::string("--quiet")) {
+            logOneGame = false;
+        } else if (argv[i] == std::string("--players")) {
+            if (i+1 >= argc) usage("Option --players requires an argument.");
+            numberOfPlayers = atoi(argv[i+1]);
+            if (numberOfPlayers < 2) usage("Invalid number of players.");
+            if (numberOfPlayers > 10) usage("Invalid number of players.");
+            ++i;
+        } else if (argv[i] == std::string("--games")) {
+            if (i+1 >= argc) usage("Option --games requires an argument.");
+            numberOfGames = atoi(argv[i+1]);
+            if (numberOfGames <= 0) usage("Invalid number of games.");
+            if ((numberOfGames % 1000) != 0) usage("Number of games must be divisible by 1000.");
+            ++i;
+        } else if (argv[i] == std::string("--every")) {
+            if (i+1 >= argc) usage("Option --every requires an argument.");
+            every = atoi(argv[i+1]);
+            if (every <= 0) usage("Invalid number for option --every.");
+            if ((every % 1000) != 0) usage("Number for option --every must be divisible by 1000.");
+            ++i;
+        } else {
+            usage("Invalid option.");
         }
+    }
+
+    std::srand(std::time(NULL));
+
+    if (logOneGame) {
+        /* Run one game with logging to stderr. */
+        Hanabi::Server server;
+        BotFactory<BOTNAME> botFactory;
+        server.setLog(&std::cerr);
+        int score = server.runGame(botFactory, numberOfPlayers);
+        std::cout << stringify(BOTNAME) " scored " << score << " points in that first game.\n";
+    }
+
+    /* Run a million games, in parallel if possible. */
+    Statistics stats = {};
+    for (int i=0; i < numberOfGames/1000; ++i) {
+        run_iterations(numberOfPlayers, 1000, stats);
+        const int games = stats.games;
+        if ((stats.games % every) == 0) {
+            dump_stats(stats);
+        }
+    }
+
+    if ((numberOfGames % every) != 0) {
+        dump_stats(stats);
     }
 
     return 0;
